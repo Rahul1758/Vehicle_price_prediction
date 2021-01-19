414564aca6b2ec9344e1b91f7641591dcde2efad
Rahul Menon <64649104+Rahul1758@users.noreply.github.com>
2021-01-19 07:31:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
-echo "\
-[general]\n\
-email = \"raon1758@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
